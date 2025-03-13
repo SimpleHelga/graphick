f686c52be2330719d1daa12a0b527296c40d8932
SimpleHelga <igortyrin034@gmail.com>
2025-03-13 17:35:04 +0400

    Add files via upload

--- lab1_1.py
+++ lab1_1.py
@@ -126,29 +126,53 @@ image = Image.fromarray(matrix, mode="RGB")
 image=ImageOps.flip(image)
 image.save("image2.png")
 '''
+import numpy as np
+Tz=1
+g=10000*Tz
+def rotate(vertices, alpha, beta, gamma):
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(alpha), -np.sin(alpha)],
+                    [0, np.sin(alpha), np.cos(alpha)]])
+    R_y = np.array([[np.cos(beta), 0, np.sin(beta)],
+                    [0, 1, 0],
+                    [-np.sin(beta), 0, np.cos(beta)]])
+    R_z = np.array([[np.cos(gamma), -np.sin(gamma), 0],
+                    [np.sin(gamma), np.cos(gamma), 0],
+                    [0, 0, 1]])
+    R = R_x @ R_y @ R_z
+
+    transformed_vertices = [R @ vertex+[0,-0.03,Tz] for vertex in vertices]
+
+    return transformed_vertices
 def bercent(x,y,x0,y0,x1,y1,x2,y2):
     lambda0 = ((x - x2) * (y1 - y2) - (x1 - x2) * (y - y2))/((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
     lambda1 = ((x0 - x2) * (y - y2) - (x - x2) * (y0 - y2))/((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
     lambda2 = 1.0 - lambda0 - lambda1
     return lambda0, lambda1, lambda2
 def draw_triange(matrix,z_buffer,x0, y0, x1, y1, x2, y2,color):
-    xmin= math.floor(min(x0,x1,x2))
+    u0 = g * x0/z0  + matrix.shape[1]/2
+    v0 = g * y0/z0  + matrix.shape[0]/2
+    u1 = g * x1/z1  + matrix.shape[1]/2
+    v1 = g * y1/z1  + matrix.shape[0]/2
+    u2 = g * x2/z2  + matrix.shape[1]/2
+    v2 = g * y2/z2  + matrix.shape[0]/2
+    xmin= math.floor(min(u0,u1,u2))
     if(xmin<0):
         xmin=0
-    ymin= math.floor(min(y0,y1,y2))
+    ymin= math.floor(min(v0,v1,v2))
     if(ymin<0):
         ymin=0
-    xmax= math.ceil(max(x0,x1,x2))
+    xmax= math.ceil(max(u0,u1,u2))
     if(xmax>matrix.shape[1]):
         xmax=matrix.shape[1]
-    ymax= math.ceil(max(y0,y1,y2))
+    ymax= math.ceil(max(v0,v1,v2))
     if(ymax>matrix.shape[0]):
         ymax=matrix.shape[0]
     for x in range(xmin,xmax):
         for y in range(ymin,ymax):
-            l0,l1,l2=bercent(x,y, x0, y0, x1, y1, x2, y2)
+            l0,l1,l2=bercent(x,y, u0, v0, u1, v1, u2, v2)
             if(l0>=0 and l1>=0 and l2>=0):
-                z=10*z0+l1*z1+l2*z2
+                z=l0*z0+l1*z1+l2*z2
                 if z<z_buffer[y,x]:
                     z_buffer[y,x]=z
                     matrix[y,x]=color
@@ -163,24 +187,28 @@ def ligth(x,y,z):
 f= open("C:\\Users\\igort\\Downloads\\Telegram Desktop\\model_1.obj")
 vec=[]
 lis=[]
+alpha,beta,gamma= np.radians(0),np.radians(180),np.radians(0)
+x,y,z=0,0,0
 for s in f:
     split=s.split()
     if(split[0]=="v"):
         vec.append([float(x) for x in split[1:]])
     if (split[0]=='f'):
         lis.append([int(x.split('/')[0]) for x in split[1:]])
-for vertex in vec:
-    matrix[int(10000*vertex[1])+1000,int(10000*vertex[0])+1000]= (139,0,255)
+transformed_vec=rotate(vec,alpha,beta, gamma)
+'''for vertex in transformed_vec:
+    matrix[int(10000*vertex[1]),int(10000*vertex[0])]= (139,0,255)'''
 for face in lis:
-    x0=10000*vec[face[0]-1][0]+1000
-    y0=10000*vec[face[0]-1][1]+1000
-    z0= 10000 * vec[face[0] - 1][2] + 1000
-    x1=10000*vec[face[1]-1][0]+1000
-    y1=10000*vec[face[1]-1][1]+1000
-    z1 = 10000 * vec[face[1] - 1][2] + 1000
-    x2=10000*vec[face[2]-1][0]+1000
-    y2=10000*vec[face[2]-1][1]+1000
-    z2 = 10000 * vec[face[2] - 1][2] + 1000
+    x0=transformed_vec[face[0]-1][0]
+    y0=transformed_vec[face[0]-1][1]
+    z0=transformed_vec[face[0] - 1][2]
+    x1=transformed_vec[face[1]-1][0]
+    y1=transformed_vec[face[1]-1][1]
+    z1=transformed_vec[face[1] - 1][2]
+    x2=transformed_vec[face[2]-1][0]
+    y2=transformed_vec[face[2]-1][1]
+    z2=transformed_vec[face[2] - 1][2]
+
     xn,yn,zn=norm(x0,y0,z0,x1,y1,z1,x2,y2,z2)
     cosl=ligth(xn,yn,zn)
     if cosl<0:
